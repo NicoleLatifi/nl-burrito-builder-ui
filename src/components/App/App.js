9f4c2697d4ab96744c7549f98c2a180dec15cd40
Nicole Latifi <Nicole.Latifi@gmail.com>
2020-09-22 10:16:03 -0600

    Add postSubmittedOrder that sends a post network request and adds the response to state

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import {getOrders} from '../../apiCalls';
+import {getOrders, postOrder} from '../../apiCalls';
 import Orders from '../../components/Orders/Orders';
 import OrderForm from '../../components/OrderForm/OrderForm';
 
@@ -12,10 +12,15 @@ class App extends Component {
     }
   }
 
-  componentDidMount() {
-    getOrders()
+  async componentDidMount() {
+    await getOrders()
       .then(data => this.setState({ orders: data.orders }))
       .catch(err => console.error('Error fetching:', err));
+  }
+
+  postSubmittedOrder = async (name, ingredients) => {
+    await postOrder(name, ingredients)
+      .then(data => this.setState({ orders: [...this.state.orders, data] }))
   }
 
   render() {
@@ -23,7 +28,7 @@ class App extends Component {
       <main className="App">
         <header>
           <h1>Burrito Builder</h1>
-          <OrderForm />
+          <OrderForm postSubmittedOrder={this.postSubmittedOrder} />
         </header>
 
         <Orders orders={this.state.orders}/>
