a0093b8da6688e38bd634cfc73162c407ab7f066
Nicole Latifi <Nicole.Latifi@gmail.com>
2020-09-22 11:15:02 -0600

    Test that it renders the orders

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,1 +1,47 @@
+import React from 'react'
+import { screen, render, fireEvent, waitFor } from '@testing-library/react'
+import App from './App'
+import { getOrders } from '../../apiCalls'
+jest.mock('../../apiCalls')
 
+describe('App Component', () => {
+  let possibleIngredients, data
+  beforeEach(() => {
+    possibleIngredients = ['beans', 'steak']
+    data = {
+      orders: [
+        {
+          name: "Pat",
+          ingredients: ['beans', 'steak']
+        }
+      ]
+    }
+  })
+  
+  it.skip('Should render the form', () => {
+    render (
+      <App />
+    )
+
+    const header = screen.getByRole('heading', { name: 'Burrito Builder' })
+    const nameInput = screen.getByPlaceholderText('Name')
+    const beansButton = screen.getByRole('button', { name: 'beans' })
+
+
+    expect(header).toBeInTheDocument
+    expect(nameInput).toBeInTheDocument
+    expect(beansButton).toBeInTheDocument
+  })
+
+  it('Should render the orders', async () => {
+    getOrders.mockResolvedValue(data)
+
+    render (
+      <App />
+    )
+    
+    const orderName = await waitFor(() => screen.getByRole('heading', { name: 'Pat' }))
+
+    expect(orderName).toBeInTheDocument
+  })
+})
