3671bd20df0a4e99a2c64c60dfdf34d0a400ed25
Nicole Latifi <Nicole.Latifi@gmail.com>
2020-09-22 11:48:14 -0600

    Test submit when name and ingredients are selected

--- src/components/OrderForm/OrderForm.test.js
+++ src/components/OrderForm/OrderForm.test.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { screen, render } from '@testing-library/react'
+import { screen, render, fireEvent } from '@testing-library/react'
 import OrderForm from './OrderForm'
 
 describe('OrderForm Component', () => {
@@ -18,17 +18,46 @@ describe('OrderForm Component', () => {
     const steakButton = screen.getByRole('button', { name: 'steak' })
     const submitButton = screen.getByRole('button', { name: 'Submit Order' })
 
-    expect(name).toBeInTheDocument
+    expect(nameInput).toBeInTheDocument
     expect(beansButton).toBeInTheDocument
     expect(steakButton).toBeInTheDocument
     expect(submitButton).toBeInTheDocument
   })
 
-  it('Should update the name and ingredients as they are entered and selected', () => {
+  it('Should update name when inputted', () => {
+    render (
+      <OrderForm />
+    )
+    
+    const nameInput = screen.getByPlaceholderText('Name')
+    
+    fireEvent.change(nameInput, { target: { value: 'Nicole' } })
 
+    expect(nameInput.value).toEqual('Nicole')
   })
 
-  it('Should fire the correct method when submitted', () => {
-{/* <OrderForm postSubmittedOrder={this.postSubmittedOrder} /> */}
+  it('Should post order when submitted if name is entered and an ingredient is selected', () => {
+    const mockPostSubmittedOrder = jest.fn()
+
+    render (
+      <OrderForm postSubmittedOrder={mockPostSubmittedOrder} />
+    )
+
+    const submitButton = screen.getByRole('button', { name: 'Submit Order' })
+    fireEvent.click(submitButton)
+    expect(mockPostSubmittedOrder).toHaveBeenCalledTimes(0)
+
+    const nameInput = screen.getByPlaceholderText('Name') 
+    fireEvent.change(nameInput, { target: { value: 'Nicole' } })
+    fireEvent.click(submitButton)
+    expect(mockPostSubmittedOrder).toHaveBeenCalledTimes(0)
+    
+    const beansButton = screen.getByRole('button', { name: 'beans' })
+    fireEvent.click(beansButton, { target: { name: 'beans' } })
+    fireEvent.change(nameInput, { target: { value: 'Nicole' } })
+    fireEvent.click(submitButton)
+
+    expect(mockPostSubmittedOrder).toHaveBeenCalledTimes(1)
+    
   })
 })
