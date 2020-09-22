959d93321a2afaf404292b610def3a881d1cb7fe
Nicole Latifi <Nicole.Latifi@gmail.com>
2020-09-22 10:15:06 -0600

    Add postOrder fetch method

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -3,3 +3,20 @@ export const getOrders = () => {
       .then(response => response.json())
       .then(data => {return data})
 }
+
+export const postOrder = (name, ingredients) => {
+  const stringifiedOrder = JSON.stringify({ name: name, ingredients: ingredients });
+  return fetch('http://localhost:3001/api/v1/orders', {
+    method: 'Post',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: stringifiedOrder,
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      return data
+    })
+}
